a95f3e89d71d0ef177cf7fca1bf3eb2fdc3cbf20
David H <dmheydt@gmail.com>
2024-08-15 12:01:43 -0400

    it works!

--- src/client.js
+++ src/client.js
@@ -3,6 +3,19 @@ var name;
 var username;
 var connectedUser;
 var otheruser;
+var activecss;
+var activejs;
+let device;
+let server;
+let characteristic;
+let xposition;
+let yposition;
+let drive;
+let dc;
+
+const deviceName = 'ESP32_BLE_Server';
+const serviceUUID = '12345678-1234-1234-1234-123456789012'; // Replace with your service UUID
+const characteristicUUID = 'abcdef12-1234-1234-1234-abcdef123456'; // Replace with your characteristic UUID
 
 //connecting to our signaling server
 //var conn = new WebSocket('ws://10.0.0.30:9090');
@@ -92,10 +105,29 @@ var getstreamsBtn = document.querySelector('#getstreamsbtn');
 var otherProfile = document.querySelector('#otherprofile');
 var liveStreams = document.querySelector("#livestreams");
 
-var hangUpBtn = document.querySelector('#hangUpBtn');
+var connectdeviceBtn = document.querySelector('#connectdevice-Btn');
+var devicedropBtns = document.querySelector('.device-dropdown-content');
+var arrowBtns = document.querySelector('.arrow-Btns');
+var disconnectdeviceBtn = document.querySelector('#disconnectdevice-Btn');
+var resetBtn = document.querySelector('#reset-Btn');
+var controlpaneloutputs = document.querySelector('.control-panel-outputs');
+var cparrowshost = document.querySelectorAll('.arrow-container-host');
+var cparrowsremote = document.querySelectorAll('.arrow-container-remote');
+
+var deviceinfo = document.querySelector('#deviceinfo');
+var controlpanel = document.querySelector('#control-panel');
 
 var localVideo = document.querySelector('#localVideo');
 var remoteVideo = document.querySelector('#remoteVideo');
+
+const forward = document.getElementById("forward");
+const left = document.getElementById("turnleft");
+const right = document.getElementById("turnright");
+const reverse = document.getElementById("reverse");
+const hostforward = document.getElementById("host-forward");
+const hostleft = document.getElementById("host-left");
+const hostright = document.getElementById("host-right");
+const hostreverse = document.getElementById("host-reverse");
 
 var yourConn;
 var stream;
@@ -111,6 +143,8 @@ function init() {
    loginPage.style.display = "block";
    homePage.style.display = "none";
    profilePage.style.display = "none";
+   xposition = 90;
+   yposition = 90;
    
    };
 
@@ -159,12 +193,11 @@ getstreamsBtn.addEventListener("click", function (event) {
          username: username
       });
    
-
 });
 
 goliveBtn.addEventListener("click", function () {
   console.log(username +" is going live");
-  navigator.webkitGetUserMedia({ video: true, audio: false }, (stream) => {
+  navigator.webkitGetUserMedia({ video: true, audio: true }, (stream) => {
       
      localStream(stream);
 
@@ -203,6 +236,28 @@ function localStream(stream) {
          }
       };
  }
+
+//open datachannel as Peer A
+function opendc() {
+   dc = yourConn.createDataChannel("PeerA");
+   // Set up event handlers
+   dc.onopen = () => {
+      console.log("Data channel A is open");
+      // Send a message once the data channel is open
+      dc.send("Hello, Peer B!");
+      controlpaneloutputs.disabled = true;
+   };
+
+   dc.onmessage = (event) => {
+      console.log("Received from Peer B:", event.data);
+      move(event.data);
+      
+   };
+
+   dc.onclose = () => {
+      console.log("Data channel A detected closure from Peer B");
+  };
+}
 // stop local stream
 endliveBtn.addEventListener("click", function (event) {
    liveVideo = 0;
@@ -277,10 +332,42 @@ spawnBtn.addEventListener("click", function (event) {
    });
    liveremoteVideo = 1;
    toggleprofile('remote');
+   dcpeerB();
+   controlpanel.style.display = "block";
+   connectdeviceBtn.style.display = "none";
+   disconnectdeviceBtn.style.display = "none";
+   resetBtn.style.display = "none";
+   cparrowsremote.forEach(cparrowsremote => {
+      cparrowsremote.style.display = 'inline-block';
+    });
 
 });
 
+function dcpeerB() {
+   // Listen for the data channel event
+   yourConn.ondatachannel = (event) => {
+      dc = event.channel;
+
+      // Set up event handlers for Peer B's data channel
+      dc.onopen = () => {
+         console.log("Data channel B is open");
+         // Respond to Peer A
+         dc.send("Hello, Peer A!");
+      };
+
+      dc.onmessage = (event) => {
+         console.log("Received from Peer A:", event.data);
+         
+      };
+
+      dc.onclose = () => {
+         console.log("Data channel B has been closed");
+     };
+   };
+}
+
 function watchStream (name) {
+   opendc();
    clientName = name;
    var callToUsername = clientName;
  
@@ -338,8 +425,6 @@ function watchStream (name) {
    }
  }
 
-////////////////////////////////BELOW IS GOOD
-
 //when somebody sends us an offer
 function handleOffer(offer) {
    //connectedUser = host;
@@ -379,6 +464,7 @@ function handleLeave() {
    if (liveVideo == 1) {
       updatelive('addlive');
       connectedUser = null;
+      dc = null;
    } 
    
    if (liveremoteVideo == 1) {
@@ -395,6 +481,7 @@ function handleLeave() {
             username: username
          });
          connectedUser = null;
+         dc = null;
       }
    }
 
@@ -406,19 +493,20 @@ function handleRemoteLeave() {
       username: username,
       othername: connectedUser
    });
+   dc.close();
+   console.log("Data channel B is closed");
+   dc = null;
    connectedUser = null;
    remoteVideo.srcObject = null;
- //  localVideo.srcObject = null;
    yourConn.close();
    yourConn.onicecandidate = null;
    yourConn.onaddstream = null;
    spawnBtn.style.display = "block";
-   
-   
-   
+
 };
 function handleFinalLeave() {
    connectedUser = null;
+   dc = null;
    remoteVideo.srcObject = null;
    localVideo.srcObject = null;
    yourConn.close();
@@ -464,55 +552,367 @@ function togglehome() {
 }
 
 function toggleprofile(msg) {
-   if (username) {
-      var data = msg;
-      profilePage.style.display = "block";
-      homePage.style.display = "none";
-      switch(data) {
-         case "local":
-            if (liveremoteVideo == 1) {
-               toggleprofile('remote');
-            } else {
-               profileTitle.innerHTML = username;
-               remoteVideo.style.display = "none";
-               localVideo.style.display = "block";
-               spawnBtn.style.display = "none";
-               endotherliveBtn.style.display = "none";
-               if (liveVideo == 1) {
-                  goliveBtn.style.display = "none";
-                  endliveBtn.style.display = "block";
-               } else {
-                  goliveBtn.style.display = "block";
-                  endliveBtn.style.display = "none";
-               }
-            }
+if (username) {
+   var data = msg;
+   profilePage.style.display = "block";
+   homePage.style.display = "none";
+   switch(data) {
+      case "local":
+         if (liveremoteVideo == 1) {
+            toggleprofile('remote');
+         } else {
+            profileTitle.innerHTML = username;
+            remoteVideo.style.display = "none";
+            localVideo.style.display = "block";
+            spawnBtn.style.display = "none";
+            endotherliveBtn.style.display = "none";
+            disconnectdeviceBtn.style.display = "none";
+            controlpaneloutputs.style.display = "none";
+            //cparrowsremote.style.display = "none";
+            cparrowsremote.forEach(cparrowsremote => {
+               cparrowsremote.style.display = 'none';
+             });
             
-            break;
-   
-         case "remote":
-            if (otheruser == username) {
-               toggleprofile('local');
-            } else {
-               profileTitle.innerHTML = otheruser;
-               remoteVideo.style.display = "block";
-               localVideo.style.display = "none";
+            if (liveVideo == 1) {
                goliveBtn.style.display = "none";
+               endliveBtn.style.display = "block";
+            } else {
+               goliveBtn.style.display = "block";
                endliveBtn.style.display = "none";
-               if (liveremoteVideo == 1) {
-                  spawnBtn.style.display = "none";
-                  endotherliveBtn.style.display = "block";
-               } else {
-                  spawnBtn.style.display = "block";
-                  endotherliveBtn.style.display = "none";
-               }
-               
             }
          }
          
-      } else {
-         init();
+         break;
+
+      case "remote":
+         if (otheruser == username) {
+            toggleprofile('local');
+         } else {
+            profileTitle.innerHTML = otheruser;
+            remoteVideo.style.display = "block";
+            localVideo.style.display = "none";
+            goliveBtn.style.display = "none";
+            endliveBtn.style.display = "none";
+            controlpanel.style.display = "none";
+            //cparrowshost.style.display = "none";
+            cparrowshost.forEach(cparrowshost => {
+               cparrowshost.style.display = 'none';
+             });
+            
+            document.getElementById('cph2').style.display = "none";
+            document.getElementById('deviceinfo').style.display = "none";
+            if (liveremoteVideo == 1) {
+               spawnBtn.style.display = "none";
+               endotherliveBtn.style.display = "block";
+            } else {
+               spawnBtn.style.display = "block";
+               endotherliveBtn.style.display = "none";
+            }
+            
+         }
+         break;
+
+      default:
+         console.log(`Error msg: ${expr}.`);
       }
+      
+   } else {
+      init();
    }
+}
+
+function toggleDevices() {
+   if (connectdeviceBtn.classList.contains("active")) {
+      devicedropBtns.style.display = "none";
+      connectdeviceBtn.classList.remove("active");      
+  } else {
+      devicedropBtns.style.display = "block";
+      connectdeviceBtn.classList.add("active");
+  }
+}
+
+function deviceSelect(option) {
+   switch(option) {
+      case "option1":
+         devicedropBtns.style.display = "none";
+         connectdeviceBtn.classList.remove("active"); 
+
+         break;
+
+      case "option2":
+         devicedropBtns.style.display = "none";
+         connectdeviceBtn.classList.remove("active"); 
+         //load js
+        // var script = document.createElement("script");
+        // script.src = "APIs/v0-robot/v0-robot.js";  // Path to the external JS file
+        // script.type = "text/javascript";
+        // script.id = "v0-robot-js"
+        // activejs = script.id;
+        // script.onload = function() {
+       //     console.log("Script loaded and executed.");
+       //  };
+       //  document.body.appendChild(script);
+         //load css
+        // var link = document.createElement("link");
+        // link.rel = "stylesheet";
+        // link.type = "text/css";
+       //  link.href = "APIs/v0-robot/v0-robot-styles.css"; 
+       //  link.id = "v0-robot-css";
+       //  activecss = link.id;
+       //  document.head.appendChild(link);
+ 
+         //connect to device
+         connectDevice();
+         
+
+         break;
+      
+      case "option3":
+      devicedropBtns.style.display = "none";
+      connectdeviceBtn.classList.remove("active"); 
+
+         break;
+
+      default:
+         console.log(`Error msg: ${expr}.`);
+   }
+}
+
+// disconnect device button
+disconnectdeviceBtn.addEventListener("click", function (event) {
+   disconnectDevice();   
+});
+
+// reset device button
+disconnectdeviceBtn.addEventListener("click", function (event) {
+   resetDevice();
+});
+
+async function connectDevice(params) {
+   try {
+       // Request a Bluetooth device
+       device = await navigator.bluetooth.requestDevice({
+           filters: [{ name: deviceName }],
+           optionalServices: [serviceUUID]
+       });
+
+       // Connect to the GATT server
+       server = await device.gatt.connect();
+
+       // Get the primary service
+       const service = await server.getPrimaryService(serviceUUID);
+
+       // Get the characteristic
+       characteristic = await service.getCharacteristic(characteristicUUID);
+
+       //enable buttons
+       connectdeviceBtn.style.display = "none";    
+       disconnectdeviceBtn.style.display = "block";          
+       controlpaneloutputs.style.display = "block";
+       cparrowshost.forEach(cparrowshost => {
+         cparrowshost.style.display = 'inline-block';
+       });
+
+       deviceinfo.innerHTML = deviceName;
+       console.log('Connected and ready to send messages.');
+   } catch (error) {
+       console.error('Connection failed:', error);
+   }
+}
+
+async function disconnectDevice(params) {
+   if (server) {
+       server.disconnect();
+       console.log('Disconnected from the device.');
+
+       // Clear the references
+       device = null;
+       server = null;
+       characteristic = null;
+
+       //update buttons
+      connectdeviceBtn.style.display = "block";
+      controlpaneloutputs.style.display = "none";
+   } else {
+       console.error('No active connection to disconnect.');
+   }
+}
+
+forward.onpointerdown = function() {
+   sendDC(2000);
+}
+forward.onpointerup = function () {
+   setTimeout(sendDC, 100, 2004);
+}
+turnleft.onpointerdown = function() {
+   sendDC(2001);
+}
+turnleft.onpointerup = function() {
+   setTimeout(sendDC, 100, 2004);
+}
+turnright.onpointerdown = function() {
+   sendDC(2002);
+}
+turnright.onpointerup = function() {
+   setTimeout(sendDC, 100, 2004);
+}
+reverse.onpointerdown = function() {
+   sendDC(2003);
+}
+reverse.onpointerup = function() {
+   setTimeout(sendDC, 100, 2004);
+}
+hostforward.onpointerdown = function() {
+   move(2000);
+}
+hostforward.onpointerup = function () {
+   move(2004);
+}
+hostleft.onpointerdown = function() {
+   move(2001);
+}
+hostleft.onpointerup = function() {
+   move(2004);
+}
+hostright.onpointerdown = function() {
+   move(2002);
+}
+hostright.onpointerup = function() {
+   move(2004);
+}
+hostreverse.onpointerdown = function() {
+   move(2003);
+}
+hostreverse.onpointerup = function() {
+   move(2004);
+}
+
+function move(string) {
+   let value = Number(string);
+   console.log(value);
+   if (value > -100 && value < 100) {
+       xposition = xposition + value;
+       if (xposition < 0) {
+           xposition = 0;
+       } if (xposition > 180){
+           xposition = 180;
+       }
+       sendpos('pan');
+   } if (value < -100 || value > 100 && value < 999) {
+       if (value < -100) {
+           yposition = yposition + (value + 100);
+       } else {
+           yposition = yposition + (value - 100);
+       }
+       if (yposition <= 0) {
+           yposition = 0;
+       } if (yposition >= 180){
+           yposition = 180;
+       }
+       sendpos('tilt');
+   } else if (value > 1999) {
+       drive = value;
+       sendpos('drive');
+   }
+   
+}
+
+async function sendpos(pantilt) {
+   switch (pantilt) {
+       case 'pan':
+           console.log('pan: ' + xposition);
+           if (!characteristic) {
+               console.error('No characteristic available. Please connect first.');
+               return;
+           }
+       
+           try {
+               // Send a message
+               const encoder = new TextEncoder();
+               const message = xposition;
+               await characteristic.writeValue(encoder.encode(message));
+       
+               console.log('Message sent to robot for tilt!');
+           } catch (error) {
+               console.error('Error sending message:', error);
+           }
+           break;
+
+       case 'tilt':
+           console.log('tilt: ' + yposition);
+           let yvalue = yposition + 1000;
+           if (!characteristic) {
+               console.error('No characteristic available. Please connect first.');
+               return;
+           }
+       
+           try {
+               // Send a message
+               const encoder = new TextEncoder();
+               const message = yvalue;
+               await characteristic.writeValue(encoder.encode(message));
+       
+               console.log('Message sent!');
+           } catch (error) {
+               console.error('Error sending message:', error);
+           }
+           break;
+
+           case 'drive':
+           console.log('drive: ' + drive);
+           
+           driveBT();
+           break;
+       
+       default:
+           console.log(`Sorry, we are out of ${expr}.`);
+   }
+}
+async function driveBT() {
+   try {
+      // Send a message
+      const encoder = new TextEncoder();
+      const message = drive;
+      await characteristic.writeValue(encoder.encode(message));
+      console.log('Message sent!');
+      return;
+   } catch (error) {
+      console.error('Error sending message:', error);
+     }
+}
+   
+
+function sendDC(value) {
+   try {
+      dc.send(value);
+      console.log(value);
+   } catch (e) {
+      console.log(e);
+   }
+   
+}
+
+async function resetDevice(params) {
+   xposition = 90;
+   yposition = 1090;
+   if (!characteristic) {
+       console.error('No characteristic available. Please connect first.');
+       return;
+   }
+
+   try {
+       // Send a message
+       const encoder = new TextEncoder();
+       const messagex = xposition;
+       const messagey = yposition;
+       await characteristic.writeValue(encoder.encode(messagex));
+       await characteristic.writeValue(encoder.encode(messagey));
+       console.log('Message sent!');
+       yposition = 90;
+   } catch (error) {
+       console.error('Error sending message:', error);
+   }
+}
 
 init();
 
